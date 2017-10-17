e86341b995352dc6948a8064cf35e71e8f454e0d
Deyan <dublinstand@gmail.com>
2017-10-17 16:06:20 +0100

    commit

--- extracting-response-api/src/main/java/com/jsonpathexample/CurrencyExchangeJsonPathExample.java
+++ extracting-response-api/src/main/java/com/jsonpathexample/CurrencyExchangeJsonPathExample.java
@@ -1,8 +1,12 @@
 package com.jsonpathexample;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.List;
 
 import static com.jayway.restassured.RestAssured.given;
 
@@ -19,7 +23,6 @@ public class CurrencyExchangeJsonPathExample {
     public void extractCountFromResponse() {
         int count = given()
                 .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
-                .param("format", "json")
                 .param("format", "json")
                 .param("diagnostics", "true")
                 .param("env", "store://datatables.org/alltableswithkeys")
@@ -40,7 +43,6 @@ public class CurrencyExchangeJsonPathExample {
         String language = given()
                 .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
                 .param("format", "json")
-                .param("format", "json")
                 .param("diagnostics", "true")
                 .param("env", "store://datatables.org/alltableswithkeys")
                 .when()
@@ -59,7 +61,6 @@ public class CurrencyExchangeJsonPathExample {
         String name = given()
                 .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
                 .param("format", "json")
-                .param("format", "json")
                 .param("diagnostics", "true")
                 .param("env", "store://datatables.org/alltableswithkeys")
                 .when()
@@ -72,5 +73,168 @@ public class CurrencyExchangeJsonPathExample {
         System.out.println("The value of the first name is: " + name);
     }
 
-    //4
+    //4 Get the values under rate
+    @Test
+    public void getRateValues(){
+        List<String> rateValues = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate");
+
+        System.out.println("The rate values are: " + rateValues);
+        System.out.println("The size of rate is: " + rateValues.size());
+    }
+
+    //5 Get the size of rate
+    @Test
+    public void getSizeOfRate(){
+        int sizeOfRate = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate.size()");
+
+        System.out.println("The size of rate is: " + sizeOfRate);
+    }
+
+    //6 Get all the names from the response
+    @Test
+    public void getAllNamesFromResponse(){
+        List<String> allNamesFromResponse = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate.Name");
+
+        System.out.println("The list of all names is: " + allNamesFromResponse);
+    }
+
+    //7 Loop through the rate collection and pick the collection with Name="USD/BRL"
+    //Get all values
+    @Test
+    public void getAllValuesInCollectionUSDBRL(){
+        List<String> name = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate.findAll{it.Name=='USD/BRL'}");
+
+        System.out.println("The values for USD/BRL are: " + name);
+    }
+
+    //8 Get the rate value for  Name="USD/EUR"
+    //Get all values
+    @Test
+    public void getRateValueForUSDEUR(){
+        List<String> rateValues = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate.findAll{it.Name=='USD/EUR'}.Rate");
+
+        System.out.println("The rate value for USD/EUR is: " + rateValues);
+    }
+
+    //9 Get the Names which have Rate greater than 30
+    @Test
+    public void getNamesWithRateGreaterThan30(){
+        Response response  = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql");
+
+        //We can get the JsonPath from the Response - first case
+        JsonPath path = response.jsonPath();
+
+        //We can get the JsonPath from the Response - second case
+        JsonPath path2 = new JsonPath(response.body().asInputStream());
+
+        //We get a list of all Names, that have a rate greater than 30
+        List<String> names = response.
+                then()
+                .extract()
+                .path("query.results.rate.findAll{it.Rate > '30'}.Name");
+
+        System.out.println("The names which have an exchange rate greater than 30 are: " + names);
+
+        //Using the jsonPath to extract value from the path that comes from the response body
+        System.out.println("First Path to get the only rate value: " + path.get("query.results.rate.findAll{it.Name=='USD/EUR'}.Rate"));
+
+        //Using the jsonPath to extract value from the path2 that comes from the response body
+        System.out.println("Second Path to get the rate as an array: " + path2.get("query.results.rate.findAll{it.Name=='USD/EUR'}"));
+
+        //Printing in a nice way the JSON response
+        System.out.println("Second Path to be printed nicely: " + path2.prettyPrint());
+    }
+
+    //10 Get all elements that start with id = USDB
+    @Test
+    public void getElementsThatStartsWithIdUsdb(){
+        List<String> values = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate.findAll{it.id==~/USDB.*/}");
+
+        System.out.println("The value that start with USDB are: " + values);
+    }
+
+    //11 Get all elements that END with id = UD
+    @Test
+    public void getElementsThatEndsWithIdUd(){
+        List<String> values = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "json")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                .extract()
+                .path("query.results.rate.findAll{it.id==~/.*UD/}");
+
+        System.out.println("The value that end with UD are: " + values);
+    }
 }
---
+++ extracting-response-api/src/main/java/com/xmlpathexamples/CurrencyExchangeXMLPathExample.java
@@ -0,0 +1,209 @@
+package com.xmlpathexamples;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.internal.path.xml.NodeChildrenImpl;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static com.jayway.restassured.RestAssured.given;
+
+
+public class CurrencyExchangeXMLPathExample {
+
+    @BeforeClass
+    public static void init() {
+        RestAssured.baseURI = "https://query.yahooapis.com";
+        RestAssured.basePath = "/v1/public";
+    }
+
+    //1 Extract count from the response
+    @Test
+    public void extractCountFromResponse() {
+        String count = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                .path("query.@yahoo:count");
+
+        System.out.println("The value of count is: " + count);
+    }
+
+    //2 Extract language value from the response
+    @Test
+    public void extractLanguageFromResponse() {
+        String lang = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                .path("query.@yahoo:lang");
+
+        System.out.println("The value of count is: " + lang);
+    }
+
+    //3 Extract "Name": "USD/THB value from the first rate element
+    @Test
+    public void extractNameFromFirstRateElement() {
+        String name = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                .path("query.results.rate[0].Name");
+
+        System.out.println("The value of count is: " + name);
+    }
+
+    //4 Get the values under rate as String
+    @Test
+    public void getValuesUnderRateAsString() {
+        String xml = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .andReturn()
+                .asString();
+        System.out.println("The value of xml is: " + xml);
+    }
+
+    //5 Get the size of rate
+    @Test
+    public void getSizeOfRate() {
+        Object object = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                .path("query.results.rate");
+
+        System.out.println("The class of the object is " + object.getClass());
+
+        NodeChildrenImpl sizeOfNode = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                .path("query.results.rate");
+
+        System.out.println("The the size of the object is: " + sizeOfNode.size());
+    }
+
+    //6 Get all the Names from the Response as String
+    @Test
+    public void getAllNamesFromResponse() {
+        String names = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                //here we get all Name nodes value under all rate nodes
+                .path("query.results.rate.findAll{it.Name}.Name")
+                .toString();
+
+        System.out.println("The value of xml is: " + names);
+    }
+
+    //7 Get all info for name USD/AUD
+    @Test
+    public void getAllInfoForGivenName() {
+        String values = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                //here we return all data in the rate node with Name = 'USD/AUD'
+                .path("query.results.rate.findAll{it.Name=='USD/AUD'}")
+                .toString();
+
+        System.out.println("All values for rate with Name == USD/AUD are: " + values);
+    }
+
+    //8 Extracting using attribute id="USDINR"
+    @Test
+    public void getRateByUsingIdAttribute() {
+        String rate = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                //here we use it.@id because in the rate node we have an id attribute that we want to equal its value
+                .path("query.results.rate.findAll{it.@id=='USDINR'}.Rate")
+                .toString();
+
+        System.out.println("All values for rate with Name == USD/AUD are: " + rate);
+    }
+
+    //9 Extracting using attribute id="USDINR" getting the Rate using ** the depth search
+    @Test
+    public void getRateByUsingIdAttributeAndDepthSearch() {
+        String rate = given()
+                .param("q", "SELECT * FROM yahoo.finance.xchange WHERE pair in (\"USDINR\", \"USDCAD\",\"USDAUD\",\"USDEUR\",\"USDBRL\")")
+                .param("format", "xml")
+                .param("diagnostics", "true")
+                .param("env", "store://datatables.org/alltableswithkeys")
+                .when()
+                //the resource we use is yql
+                .get("/yql")
+                .then()
+                //here we extract the path query.count that returns integer
+                .extract()
+                //here we use it.@id because in the rate node we have an id attribute that we want to equal its value
+                .path("**.findAll{it.@id=='USDINR'}.Rate")
+                .toString();
+
+        System.out.println("All values for rate with Name == USD/AUD are: " + rate);
+    }
+}
