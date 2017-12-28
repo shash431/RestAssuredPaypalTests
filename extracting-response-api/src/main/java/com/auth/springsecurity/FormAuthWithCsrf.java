9d6ec917adbbfa1076a4deaf42cb7d0dd248e843
Deyan <dublinstand@gmail.com>
2017-12-28 11:40:50 +0000

    commit

--- extracting-response-api/src/main/java/com/auth/springsecurity/FormAuthWithCsrf.java
+++ extracting-response-api/src/main/java/com/auth/springsecurity/FormAuthWithCsrf.java
@@ -1,4 +1,34 @@
 package com.auth.springsecurity;
 
+import io.restassured.RestAssured;
+import io.restassured.authentication.FormAuthConfig;
+import io.restassured.filter.session.SessionFilter;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 public class FormAuthWithCsrf {
+
+    private static SessionFilter sessionFilter;
+
+    @BeforeClass
+    public static void init() {
+        sessionFilter = new SessionFilter();
+
+        RestAssured.baseURI = "http://localhost:8085";
+        RestAssured.given().auth().form("user", "user", new FormAuthConfig("/login", "uname", "pwd")
+                .withAutoDetectionOfCsrf())
+                .filter(sessionFilter)
+                .get();
+
+        System.err.println(sessionFilter.getSessionId());
+    }
+
+    @Test
+    public void getAllStudents() {
+        RestAssured.given()
+                .sessionId(sessionFilter.getSessionId())
+                .get("/student/list")
+                .then()
+                .log().all();
+    }
 }
